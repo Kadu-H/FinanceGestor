f6ddbf7ae368fad1691ee96b15b15258547d1d85
Kdu <olocgame@gmail.com>
2023-08-22 21:58:17 -0300

    New routes in goals

--- routes/RouteGoals.js
+++ routes/RouteGoals.js
@@ -4,8 +4,12 @@ const router = express.Router();
 const {
     createGoal,
     getAllGoals,
+    getGoal,
+    updateGoal,
+    deleteGoal,
 } = require("../src/controllers/ControllerGoals.js");
 
 router.route('/').get(getAllGoals).post(createGoal);
+router.route('/:id').get(getGoal).patch(updateGoal).delete(deleteGoal);
 
 module.exports = router;
--- src/controllers/ControllerGoals.js
+++ src/controllers/ControllerGoals.js
@@ -19,7 +19,49 @@ const createGoal = async (req, res) => {
     }
 }
 
+const getGoal = async (req, res) => {
+    try{
+        const goal = await modelGoals.findById(req.params.id);
+        if(!goal){
+            res.status(404).json({ msg:`No goal with id ${req.params.id}` });
+        } else{
+            res.status(200).json({goal});
+        }
+    } catch(error){
+        res.status(500).json({ msg:error });
+    }
+}
+
+const updateGoal = async (req, res) => {
+    try{
+        const goal = await modelGoals.findByIdAndUpdate(req.params.id, req.body, { new:true, runValidators:true });
+        if(!goal){
+            res.status(404).json({ msg:`No goal with id ${req.params.id}` });
+        } else{
+            res.status(200).json({goal});
+        }
+    } catch(error){
+        res.status(500).json({ msg:error });
+    }
+}
+
+const deleteGoal = async (req, res) => {
+    try{
+        const goal = await modelGoals.findByIdAndDelete(req.params.id);
+        if(!goal){
+            res.status(404).json({ msg:`No goal with id ${req.params.id}` });
+        } else{
+            res.status(200).json({goal});
+        }
+    } catch(error){
+        res.status(500).json({ msg:error });
+    }
+}
+
 module.exports = {
     createGoal,
     getAllGoals,
+    getGoal,
+    updateGoal,
+    deleteGoal,
 }
